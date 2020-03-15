de16601d4088f009103953aa7d6f9c30966574aa
Conor Heine <conor.heine@gmail.com>
2020-03-14 17:42:31 -0700

    Update Docker configs

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,25 @@
 FROM python:3.7
 
 WORKDIR /usr/src/knock_api
-ENV PYTHONPATH=/usr/src/knock_api
+#ENV PYTHONPATH=/usr/src/knock_api
 
-COPY requirements.txt ./
-RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install gunicorn[gevent]
 
+COPY setup.py setup.cfg README.md ./
 COPY knock_api knock_api/
+COPY migrations migrations/
 
-CMD python ./knock_api/app.py
+RUN python setup.py install
 
-EXPOSE 8080
+EXPOSE 5000
+
+CMD gunicorn \
+  --worker-class gevent \
+  --workers 4 \
+  --bind 0.0.0.0:5000 \
+  --log-level info \
+  --max-requests 10000 \
+  --timeout 5 \
+  --keep-alive 5 \
+  'knock_api:create_app()'
 
--- knock_api/__init__.py
+++ knock_api/__init__.py
@@ -1,20 +1,36 @@
-# import os
+import os
+import logging
 from flask import Flask
 from flask_migrate import Migrate
 from .models import db
 
 
+logging.basicConfig(level=logging.DEBUG,
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    handlers=[logging.StreamHandler()])
+
+logger = logging.getLogger()
+
+
 def create_app(config_overrides=None):
+    logger.info(f'Starting Flask app: {__name__}')
+
+    default_db_uri = 'sqlite:////tmp/knock.db'
+    db_uri = os.environ.get('KNOCK_DB_URI') or default_db_uri
     app = Flask(__name__)
     app.config.from_mapping(
         SQLALCHEMY_TRACK_MODIFICATIONS=False,
-        SQLALCHEMY_DATABASE_URI='sqlite:////tmp/knock.db',
+        SQLALCHEMY_DATABASE_URI=db_uri,
         SECRET_KEY='knockknockknock'
-        # SQLALCHEMY_DATABASE_URI=os.environ['KNOCK_API_DB_URI']
     )
 
     if config_overrides:
         app.config.from_mapping(config_overrides)
+
+    if app.env == 'production' and not os.environ.get('KNOCK_DB_URI'):
+        logger.error('Missing KNOCK_DB_URI for production environment!')
+
+    logger.info(f'Configured for environment: {app.env}')
 
     from . import thread
     app.register_blueprint(thread.bp)
